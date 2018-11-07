ebfeadbc2f18ade86aa46bf2afba524620f257b6
Mike Grizzanti <mike.grizzanti@gmail.com>
2018-11-07 17:08:47 +0100

    Links in templates adapted to server

--- controller/controller.php
+++ controller/controller.php
@@ -9,29 +9,29 @@ class controller {
         $this->generatePage($action);
     }
     
-    public function home_DE () {
+    public function home_de () {
         $this->addContext("template", "home_de");
     }
     
-    public function contact_DE () {
+    public function kontakt_de () {
         $this->addContext("template", "kontakt_de");
     }
     
-    public function dieTischlerei_DE () {
+    public function dieTischlerei_de () {
         $this->addContext("template", "die_Tischlerei_de");
     }
     
-    public function planung3D_DE () {
+    public function planung3D_de () {
         $this->addContext("template", "3d_planung_de");
     }
     
-    public function referenzen_DE () {
+    public function referenzen_de () {
         $this->addContext("template", "referenzen_de");    
     }
     
     private function generatePage($template){
         extract($this->context);
-        require_once 'view/de'.$template.".tpl.html";
+        require_once 'view/de/'.$template.".tpl.html";
     }
     
     private function addContext($key, $value) {
